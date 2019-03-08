1cb09d69af881da2a39178eedb2e4cc2995988ee
Alison Reilly <reillyamr@gmail.com>
2019-03-08 23:51:44 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
+  has_many :characters 
+  has_many :shows, through: :characters
+  
+  def full_name 
+    first_n = self.first_name 
+    last_n = self.last_name
+    
+    "#{first_n} #{last_name}"
+  end
+  
+  def list_roles
+    actor_shows = self.shows.pluck(:name) 
+    actor_chars = self.characters.pluck(:name)
+    flatten_result = actor_chars.push(actor_shows).flatten
+    join_result = flatten_result.join(" - ")
+    
+    
+  end 
+  
+  
   
 end
