400fc3417f335ede1ac37db92d490a6ce204846f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-18 18:40:00 +0000

    Automatically backed up by Learn

--- db/migrate/20190418183753_create_characters.rb
+++ db/migrate/20190418183753_create_characters.rb
@@ -1,6 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |t|
+      t.string :name
       t.string :catchphrase
       t.integer :actor_id
       t.integer :show_id
