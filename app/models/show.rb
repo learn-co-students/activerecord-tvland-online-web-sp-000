b97ffce211d8a45f771a98bebdb99dad48f775f6
Bry Cox <carl.cc@gmail.com>
2019-05-22 06:45:30 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,7 @@
 class Show < ActiveRecord::Base
+
+  belongs_to :network
   has_many :characters
   has_many :actors, through: :characters
-
-  def build_network(args)
-    binding.pry
-  end
 
 end
--- db/migrate/007_add_more_to_characters.rb
+++ db/migrate/007_add_more_to_characters.rb
@@ -1,8 +1,7 @@
 class AddMoreToCharacters < ActiveRecord::Migration[4.2]
-  #to make an additive change to a schema we create
-  #a new migration, and then in the change method,
-  #we make the change.
+
   def change
     add_column :characters, :catchphrase, :string
   end
+
 end
