b2d23f654febb76640d70e8d3235e0ed22c8b45e
Ramon Chavez <rachavez1974@gmail.com>
2019-07-02 14:11:42 -0400

    Finish activerecord-tvland-online-web-sp-000

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -28,9 +28,7 @@ describe Character do
     urkel.catchphrase = "Did I do that?"
     urkel.save
     expect(Character.find_by(:id => urkel.id).catchphrase).to eq(urkel.catchphrase)
-
     expect(urkel.say_that_thing_you_say).to eq("#{urkel.name} always says: #{urkel.catchphrase}")
-
   end
 
   it "can build its associated show" do
