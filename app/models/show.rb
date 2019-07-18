e31ce94e3bf3e473059568ca401e8b4b7e7cadd4
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-18 23:44:57 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
   
+  def actors_list
+    self.characters.collect { |character| character.actor.full_name }
+  end
+
 end
