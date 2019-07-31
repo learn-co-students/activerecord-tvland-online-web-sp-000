5dc61c2214fcc10d94ead3beb71f8c690c4a3643
kevin lang <langkevina@gmail.com>
2019-07-31 23:34:39 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " "  + self.last_name
+  end
+
+  def list_roles
+    # expect(emilia.list_roles).to include("Khaleesi - Game of Thrones")
+    arr = []
+    self.characters.each do |item|
+      arr << item.name + " - " + item.show.name
+    end
+    arr
+  end
 end
--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -19,17 +19,18 @@ describe Character do
     niles = Character.new(name: "Niles Crane")
     niles.show = frasier
     niles.save
-    
+
     frasier.reload
     expect(frasier.characters).to include(niles)
     expect(niles.show).to eq(frasier)
   end
 
   it "has a catchphrase" do
-    
+
     urkel = Character.new(name: "Steve Urkel")
     urkel.catchphrase = "Did I do that?"
     urkel.save
+        # binding.pry
     expect(Character.find_by(id: urkel.id).catchphrase).to eq(urkel.catchphrase)
 
     expect(urkel.say_that_thing_you_say).to eq("#{urkel.name} always says: #{urkel.catchphrase}")
