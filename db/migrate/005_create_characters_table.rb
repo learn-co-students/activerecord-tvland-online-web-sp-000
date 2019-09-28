31cbd454e18723a35c798fbcb5edfa4fabf2957a
Austin Asbury <acasbury25@aol.com>
2019-09-28 03:30:02 +0000

    Done.

---
+++ db/migrate/005_create_characters_table.rb
@@ -0,0 +1,9 @@
+class CreateCharactersTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :characters do |a|
+      a.string :name
+      a.integer :actor_id
+      a.integer :show_id
+    end
+  end
+end
