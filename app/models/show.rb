f7847e55d96f9416d0061642c27f7fa92d7eef3d
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-09 03:05:30 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+    actorNames = []
+    self.characters.each do |i|
+      actorNames << i.actor.full_name unless actorNames.include?(i.actor.full_name)
+    end
+    actorNames
+  end
 end
