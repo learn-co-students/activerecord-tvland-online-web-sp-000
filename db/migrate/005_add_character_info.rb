83e8c663abeaf6b9ea43fe348f983f8ab14d3acf
David Strauch <dmaster18@gmail.com>
2019-10-31 19:16:39 +0000

    Done.

---
+++ db/migrate/005_add_character_info.rb
@@ -0,0 +1,7 @@
+class AddCharacterInfo < ActiveRecord::Migration[5.1]
+  def change
+    add_column :characters, :name, :string
+    add_column :characters, :actor_id, :integer
+    add_column :characters, :show_id, :integer
+  end
+end
