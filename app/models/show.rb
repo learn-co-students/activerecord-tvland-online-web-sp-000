9f3eed4145717165f312dca43e43918208044509
David Strauch <dmaster18@gmail.com>
2019-11-04 21:27:41 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
   
   def actors_list
     characters = Character.all
@@ -8,4 +9,5 @@ class Show < ActiveRecord::Base
       Actor.find(character.actor_id).full_name
     }
   end
+  
 end
