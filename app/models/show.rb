dafb5b38b4ab909599691b1489dd079ee92ef1b1
Christopher Dent <christopherdent01@gmail.com>
2019-11-10 22:40:08 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.map do |a|
+      a.actor.full_name 
+    end 
+  end
+
+
+
 end
+
+#Person.where("administrator = 1")
