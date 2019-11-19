ae71d4f9992d0933539a2a343a40a379e23adf31
Blythe Pollard <blytheepollard@gmail.com>
2019-11-19 15:58:05 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  belongs_to :show
   
+  def full_name
+    first = self.first_name
+    last = self.last_name
+    "#{first} #{last}"
+  end
+  
+  def list_roles
+    #lists all characters alongside show that actor is in
+  end
 end
