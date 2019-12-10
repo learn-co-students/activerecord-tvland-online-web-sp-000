f0313ebb9b39bf726c050209b2ca11bd1025f148
github username <github email address>
2019-12-10 12:31:49 -0700

    Done.

--- db/migrate/007_add_day_of_week_genre_and_season_to_show.rb
+++ db/migrate/007_add_day_of_week_genre_and_season_to_show.rb
@@ -1,4 +1,4 @@
-class AddDayOfWeekGenreAndSeasonToShow <ActiveRecord::Migration[5.1]
+class AddDayOfWeekGenreAndSeasonToShow <ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :genre, :string
