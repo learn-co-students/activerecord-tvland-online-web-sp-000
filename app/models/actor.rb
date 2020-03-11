23697f57db4c835898bbb013774643d72716f844
ksummerill <kaitlynsummerill@gmail.com>
2020-03-11 07:20:44 -0700

    complete AR Associations part 2

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  # make associations here
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  # can list all of its shows and characters
+  def list_roles
+    self.characters.collect do |character|
+       "#{character.name} - #{character.show.name}"
+     end
+  end
 end
