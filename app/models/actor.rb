00244e76368a36a0dc0f0f49ad72441b64493758
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-12 19:58:08 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :show, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    actor_role=[]
+    actor_role<<characters.map{|c| c.name}
+    actor_role<<show.map{|s| s.name}
+    actor_role
+    actor_role.compact.join(" - ")
+  end
+
 end
