beb4333a3d5a338f72c6b7e97873c39612d5b7e5
github username <github email address>
2020-04-11 19:39:37 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+
+    def build_network(call_letters:)
+        self.network = Network.create(call_letters: call_letters)
+    end
+
+    def actors_list
+        self.characters.map {|character| character.actor.full_name}
+    end
 end
