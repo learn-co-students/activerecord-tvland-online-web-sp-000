b7267df9fc59876a4d1c970b58f3e43975866d53
Mark A Guza <guzamaguza@gmail.com>
2020-04-20 04:08:38 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,14 +1,19 @@
 class Actor < ActiveRecord::Base
+  #associations
   has_many :characters
   has_many :shows, through: :characters
 
   def full_name
-    self.first_name
-    self.last_name
+    return "#{self.first_name} #{self.last_name}"
   end
 
   def list_roles
-    puts "#{self.character.name}  -  #{self.character.show.name}"
+    role_array = []
+    self.characters.each do |ch|
+      name_and_char = "#{ch.name} - #{ch.show.name}"
+      role_array << name_and_char
+    end
+    return role_array
   end
 
 end
