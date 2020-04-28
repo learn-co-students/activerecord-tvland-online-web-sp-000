2ed97bbd869f757d9ee9877956e405815d173ec1
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-28 18:07:11 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+  belongs_to :network 
+  has_many :characters
+  has_many :actors, through: :characters
   
+  def actors_list 
+    actors_names = [] 
+    self.actors.each do |name|
+      new_array << name.full_name
+    end 
+    return actors_names
+  end 
 end
