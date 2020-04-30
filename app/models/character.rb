cee5282fe3bf2d61e44d79588f6fb6c01857b5c5
zchesny <z.chesny@gmail.com>
2020-04-30 10:05:32 -0700

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor 
+    belongs_to :show
+
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
+
+    def build_show(show_hash)
+        Show.find_or_create_by(show_hash).tap{|show| show.characters << self}
+    end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters 
+    belongs_to :network
+
+    def actors_list 
+        self.actors.collect{|actor| actor.full_name}
+    end
+
+    def build_network(network_hash)
+        Network.find_or_create_by(network_hash).tap{|network| network.shows << self}
+    end
   
 end
