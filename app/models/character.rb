20d3cdea2c0f8b1fc7d24a930d4b4db9be508b51
zchesny <z.chesny@gmail.com>
2020-04-30 10:15:06 -0700

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,8 +6,9 @@ class Character < ActiveRecord::Base
         "#{self.name} always says: #{self.catchphrase}"
     end
 
-    def build_show(show_hash)
-        Show.find_or_create_by(show_hash).tap{|show| show.characters << self}
-    end
+    # I custom built this method, but it is already predefined with active_record associations 
+    # def build_show(show_hash)
+    #     Show.find_or_create_by(show_hash).tap{|show| show.characters << self}
+    # end
 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -7,8 +7,9 @@ class Show < ActiveRecord::Base
         self.actors.collect{|actor| actor.full_name}
     end
 
-    def build_network(network_hash)
-        Network.find_or_create_by(network_hash).tap{|network| network.shows << self}
-    end
+    # I custom built this method, but it is already predefined with active_record associations 
+    # def build_network(network_hash)
+    #     Network.find_or_create_by(network_hash).tap{|network| network.shows << self}
+    # end
   
 end
