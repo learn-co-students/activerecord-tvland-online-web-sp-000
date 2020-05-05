72a8aae51bca6d913c8b910b2c9a9d9964deaee2
cantstopcoding <jonathan_koss@yahoo.com>
2020-05-05 14:20:03 -0400

    Done.

--- app/models/network.rb
+++ app/models/network.rb
@@ -5,3 +5,5 @@ class Network < ActiveRecord::Base
     "We're sorry about passing on John Mulaney's pilot"
   end
 end
+
+
--- db/migrate/006_add_catchphrases_to_characters.rb
+++
@@ -1,8 +0,0 @@
-class AddCatchphrasesoCharacters < ActiveRecord::Migration[5.1]
-    #to make an additive change to a schema we create
-    #a new migration, and then in the change method,
-    #we make the change.
-    def change
-      add_column :characters, :catchphrases, :integer
-    end
-  end
