b429188e92fa1382e38e5c8f53189439e73c589c
ArslanInfinite <ali.arslan319@gmail.com>
2020-05-15 15:49:08 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
   
+  belongs_to :show
+  belongs_to :actor 
+
+  def say_that_thing_you_say
+    Character.all.collect do |character| 
+    "#{character.name} always says: #{character.catchphrase}"
+    end
+  end 
+
 end
