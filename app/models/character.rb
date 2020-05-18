794382a943238c052a4995f24a6c1d4103e6aa6f
ArslanInfinite <ali.arslan319@gmail.com>
2020-05-17 22:18:20 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,9 +4,10 @@ class Character < ActiveRecord::Base
   belongs_to :actor 
 
   def say_that_thing_you_say
-    Character.all.collect do |character| 
-    "#{character.name} always says: #{character.catchphrase}"
-    end
-  end 
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  # def build_network 
+  # end 
 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -5,9 +5,11 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def actors_list
+    self.actors.collect do |actor|
+      actor.full_name 
+    end
   end 
 
-  def build_network
-  end 
+  
   
 end
