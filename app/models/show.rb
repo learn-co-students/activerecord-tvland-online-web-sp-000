ba31890d696f79265f6b5f5df5d26282a1030b09
chienleow <chienleow1@gmail.com>
2020-05-22 10:27:31 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,12 @@
 class Show < ActiveRecord::Base
     has_many :characters
-    has_many :actors, through: :characters 
+    has_many :actors, through: :characters
+    belongs_to :network
   
+    def actors_list
+        #returns an array of the full names of each actor associated with the a show
+        self.actors.map {|actor|actor.full_name}
+        # binding.pry
+    end
+
 end
