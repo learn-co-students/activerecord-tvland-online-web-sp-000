252adb066e5bf7f89c4f457619d203fe4e8c62aa
micahjwhite <micahjwhite@gmail.com>
2020-05-21 21:18:22 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+
+    def say_that_thing_you_say
+        #'Tyrion Lannister always says: A Lannister always pays his debts'
+        self.name + " always says: " + self.catchphrase
+    end
 end
