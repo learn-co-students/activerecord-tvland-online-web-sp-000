f97189e6a823e9cc2712e8800bddb4ba5abd283d
gabejoseph <gabriel.joseph4@gmail.com>
2020-05-29 16:04:27 -0400

    Done.

--- db/migrate/006_add_catchphrase_to_character.rb
+++
@@ -1,6 +0,0 @@
-class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
-
-    def change 
-    end 
-    
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 8) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 8) do
     t.string "name"
     t.string "actor_id"
     t.string "show_id"
-    t.integer "genre"
+    t.string "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
@@ -32,8 +32,9 @@ ActiveRecord::Schema.define(version: 8) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
-    t.string "day"
     t.string "season"
+    t.string "day"
+    t.string "genre"
   end
 
 end
