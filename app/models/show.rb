23d44a993e1e104918144b704d02b7a06cb6001d
AustinRhoads <austinrrhoads@ymail.com>
2020-06-07 00:10:41 -0500

    wtf

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+
+  def actors_list
+    self.actors.collect {|a| a.full_name}
+  end
+
+  def build_network(splork)
+
+  end
 end
