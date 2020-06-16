59330de551395691bc9b3ac5c9beeb6dcef86e0b
mbade1 <mbade314@gmail.com>
2020-06-16 14:15:33 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    #lists the first and last name of an actor
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    #lists all of the characters that actor has 
+    #alongisde the show that the character is in.
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+  #returns an array of the full names of each actor associated with a show
+    self.characters.collect do |actor_names|
+      "#{actor_names.actor.full_name}"
+    end
+  end
 end
+
