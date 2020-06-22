57d9095486006e645cb62d7da2310d1ca344e382
jake0988 <jake098877@gmail.com>
2020-06-21 21:29:29 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :shows, through: :characters
+  has_many :characters
+
+  def full_name
+    f = self.first_name
+    l = self.last_name
+    full = "#{f} #{l}"
+  end
+
+  def list_roles
+    c = Character.where(actor_id: self.id)
+     self.characters = c
+    c[0].actor = self
+     roles = c.map do |character|
+     Show.where(id: c[0].show_id)
+     end
+     roles.flatten!
+
+    # roles[0].characters
+    c[0].show = roles[0]
+    c[0].save
+    name = c[0].name
+
+    role = roles[0].name
+    join = "#{name} - #{role}"
+    array = []
+    array << join
+       array
+    
+  end
 end
