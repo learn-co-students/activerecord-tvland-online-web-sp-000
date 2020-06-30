eecfa1f6f530c89c3eece1f733e1657c589c2f0a
ting682 <tchung682@gmail.com>
2020-06-30 10:46:46 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    list = []
+    self.characters.each {|character|
+        #binding.pry
+        list << "#{character.actor.first_name} #{character.actor.last_name}"
+    }
+    list
+  end
 end
