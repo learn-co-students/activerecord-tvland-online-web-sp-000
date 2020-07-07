0e4f290edd0b86436c5ba67ef11aa26f0bfb9125
yarvinh <yarvinhhernandez@gmail.com>
2020-07-07 11:01:52 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    # has_many :actors
+    has_many :characters
+    belongs_to :network
+    def build_network(call_letters:)
+        new_network = Network.create(call_letters: call_letters)
+        self.network = new_network
+    end
+
+    def actors_list
+      self.characters.map{|e| "#{e.actor.first_name} #{e.actor.last_name}"}
+    end
 end
