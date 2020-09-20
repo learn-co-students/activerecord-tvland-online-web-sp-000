f5a0882c0c0b8fe9eae686448e35296a7374bdd5
khoinguyenkc <khoinguyenkc@gmail.com>
2020-09-20 12:49:01 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,24 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :chracters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end 
+
+  def list_roles
+    #expected results format: ['Tyrion Lannister - Game of Thrones', 'Dwarf - Lord of the Rings']
+    
+    #does find by and where support asso attributes or just column names?
+    #yes they do support.
+    #where returns an array whereas find by only returns the first one thing found, i believe
+    characters = Character.where(actor: self)  
+    resultarray = []
+    characters.each do | char |
+         resultarray << "#{char.name} - #{char.show.name}"
+    end
+    resultarray
+  end
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+    #even iwth a s has many through relationship, u can just get it as if its a has many relationship
+    array = []
+    self.actors.each do | actor |
+        array << actor.full_name
+    end
+    array 
+  end
+
+
 end
