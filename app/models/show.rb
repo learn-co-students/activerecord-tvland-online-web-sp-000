8175140e9f39c4dfc72120de82b378c455cc438e
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-24 13:03:15 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+    list = []
+    self.actors.each do |t|
+      list << t.full_name
+    end
+    list
+  end
 end
