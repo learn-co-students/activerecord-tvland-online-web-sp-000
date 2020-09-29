f82db88ce404f7a0570511de3bf16a5b7510ed2c
jrog20 <jaimerogstad@gmail.com>
2020-09-29 16:35:18 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,9 @@
-require 'pry'
 class Show < ActiveRecord::Base
+  has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
 
   def actors_list
-    # returns an Array of the full names of each actor associated with the show.
     self.actors.map {|a| a.full_name}
-    binding.pry
   end
 end
