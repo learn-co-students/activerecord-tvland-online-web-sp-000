42fe87eb32df0b5ee485f15d1338fe5377bbee18
tsbrun <tsbrun@icloud.com>
2020-10-05 12:26:39 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,12 +1,14 @@
 class Show < ActiveRecord::Base
     has_many :characters
     belongs_to :network
+    has_many :actors, through: :characters
 
     def actors_list
-        self.characters.collect do |character|
-            character.actor.full_name
-        end
+        # this is what I needed to do before I set up the has_many_through relationship
+        # self.characters.collect do |character|
+        #     character.actor.full_name
+        # end
 
-        # self.actors.map { |actor| actor.full_name }
+        self.actors.map { |actor| actor.full_name }
     end
 end
