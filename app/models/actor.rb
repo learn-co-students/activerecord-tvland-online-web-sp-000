38f3c4fc993cbe1d6ed775e9b638b1a7146374f5
nlanese21 <nlanese21@gmail.com>
2020-10-08 01:10:25 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters 
+
+    def full_name
+        return "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        rArray = []
+        Character.all.each do | selected |
+            if selected.actor == self
+                rArray << "#{selected.name} - #{selected.show.name}"
+            end
+        end
+        return rArray
+    end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :shows
+    belongs_to :network
+
+    def actors_list
+        rArray = []
+        Actor.all.each do | selected |
+            if selected.shows.include?(self)
+                rArray << selected.full_name
+            end
+        end
+        return rArray
+    end
   
 end
