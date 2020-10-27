636f286892b5f47ab88c4b277ca362f98bdd0f1a
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2020-10-27 16:19:26 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name # returns the first and last name of an actor
+    # binding.pry
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles # lists all of the characters (in an array) that actor has; alongside the show that the character is in
+
+  end
 end
