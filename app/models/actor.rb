0cb0282a8dc38537ac21725906e74c8c5e041a20
rmholsen <jaguar.kitty@gmail.com>
2020-10-30 17:19:41 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end 
+
+  def list_roles
+    characters = []
+    self.characters.each do |c|
+        characters << "#{c.name} - #{Show.find_by(id: c.show_id).name}"
+        #Test wants "Character - show" format
+        #c.name obv is the name belonging to the character
+        #Second part is ugh
+        #Show object . find the right object by (id = this iteration of the character . show_id it belongs to) . name of the show
+    end 
+    characters
+  end 
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors
+
+  def actors_list
+    #Sadly there is no direct actors-shows link as this is built SO HERE WE GO
+    actors = []
+    #Empty actors array
+    self.characters.each do |c|
+    #Each show has characters so we're iterating over the characters array
+        actor = Actor.find_by(id: c.actor_id)
+        #For each character find the actor by the actor id for the character, assign it as a variable
+        actors << "#{actor.first_name} #{actor.last_name}" 
+        #Push that object variable's first name and last name into the actors array
+    end 
+    actors
+    #Boom, return the array
+  end 
 end
