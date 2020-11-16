3565cd429b3443f41727f5f4e5f58652cd0a79a8
John M. Stassen <john.stassen@gmail.com>
2020-11-16 11:30:15 -0800

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+   has_many :characters
+   has_many :actors, through: :characters
+   belongs_to :network
   
+    def actors_list
+    array = []
+      self.actors.each do |a|
+        array << a.full_name
+      end
+    array
+    end
 end
