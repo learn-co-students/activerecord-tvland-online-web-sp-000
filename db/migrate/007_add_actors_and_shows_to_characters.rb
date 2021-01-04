2a8872bc2aa0e479862118bdc171212486e17c5f
Christerle Nounoute <christerlesn@gmail.com>
2021-01-04 10:46:45 +0000

    Done.

---
+++ db/migrate/007_add_actors_and_shows_to_characters.rb
@@ -0,0 +1,6 @@
+class AddActorsAndShowsToCharacters < ActiveRecord::Migration[5.1]
+  def change
+    add_column :characters, :actor_id, :integer
+    add_column :characters, :show_id, :integer
+  end
+end
