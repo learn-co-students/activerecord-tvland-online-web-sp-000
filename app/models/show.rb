b7f1da0c04ee356f8d3b247f73ce420f3615f8b1
clone18476 <cameroncousins18@gmail.com>
2021-01-07 23:42:41 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
-end
+
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        characters.map do |character| #iterate through all characters for this show
+            "#{character.actor.full_name}" #map character's actor's full_name method 
+        end
+    end
+
+end 
