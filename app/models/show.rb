cb9cf547da7532ad291148300affdfb651765f38
Alison Williams <apw5011@gmail.com>
2021-01-23 18:44:25 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.map do |c|
+        c.actor.full_name
+    end
+  end 
+
+  def build_network(hash)
+    network = Network.new(hash)
+    self.network = network
+    self.network.call_letters
+  end 
 end
