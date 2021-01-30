f515d6915f69ab05793906c9afe59cd2efb38323
Allison _ <allimacq@gmail.com>
2021-01-30 21:50:45 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+  
+  def list_roles
+    roles = [ ]
+    self.characters.each do |c|
+      show = Show.find_or_create_by(id: c.show_id)
+      roles << "#{c.name} - #{show.name}"
+    end
+    roles
+  end
+
 end
