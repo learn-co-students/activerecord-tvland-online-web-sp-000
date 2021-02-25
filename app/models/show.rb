4ab476f355c8d0d910b97f58a34080faaa49705e
Jess Pesale <jess_pesale@aol.com>
2021-02-24 22:03:31 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,21 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :network
+
+
+    def build_network(show)
+        # binding.pry
+        Network.create(show)
+    end
+
+
+    # def actors_list
+    #     shows = Actor.all.collect do |s|
+    #         s.show == self
+    #     end
+    #     shows.collect do |c|
+    #         c.full_name
+    #     end
+    # end
   
 end
