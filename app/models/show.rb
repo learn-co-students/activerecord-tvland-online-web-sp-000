61ac7121033819777d31430c1f91df2bff9fafa2
Clay Stewart <c.stewart89@gmail.com>
2021-06-15 04:08:58 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+
+  def actors_list
+    self.characters.map do |character|
+        character.actor.full_name
+    end
+  end
 end
